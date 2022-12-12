4ad66f394f8bf56f2940124a07c2a66ac3354e2a
ellenmcintyre123 <g00389482@atu.ie>
2022-12-12 09:21:53 +0000

    ShiftWorker class

---
+++ src/main/java/ie/atu/ShiftWorker.java
@@ -0,0 +1,61 @@
+package ie.atu;
+
+public class ShiftWorker {
+        private String name;
+        private String gender;
+        private String shift;
+        private String age;
+
+    public ShiftWorker(String name, String gender, String shift, String age) {
+        this.name = name;
+        this.gender = gender;
+        this.shift = shift;
+        this.age = age;
+    }
+
+    public ShiftWorker() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "ShiftWorker{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", shift='" + shift + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/ie/atu/ShiftWorkerApp.java
@@ -0,0 +1,10 @@
+package ie.atu;
+
+public class ShiftWorkerApp {
+    public static void main(String[] args) {
+        ShiftWorker dummy = new ShiftWorker();
+        dummy.setName("dummy");
+
+        System.out.println("details " +dummy.toString());
+    }
+}
